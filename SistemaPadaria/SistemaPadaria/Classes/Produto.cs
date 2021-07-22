82120ee1c6a4df2eed4a3c73ed1b293b7151c3a7
Ana Viana <67766752+anv1na@users.noreply.github.com>
2021-07-21 22:09:29 -0300

    Inicializando o Projeto
    Importação do Projeto

---
+++ SistemaPadaria/SistemaPadaria/Classes/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Produto
+{
+    public Produto()
+    {
+        public int IdProduto;
+    public string NomeProduto;
+    public string MarcaProduto;
+    public bool Tipo; //Produtos para revenda || para utilização na fabricação 
+    public float Peso;
+    public decimal PreçoUnitario;
+    public string UnidadeMedida;
+    public DateTime Validade;
+}
+
+    public void AdicionarProduto()
+{
+
+}
+
+    public void AlterarProduto()
+{
+
+}
+
+    public void ConsultarProduto()
+{
+
+}
+
+    public void InativarProduto()
+{
+        
+}
+
+}
