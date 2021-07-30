b29159babda288701954082636f64bd8da12ef91
Barbara Barbosa <baba_gomes@hotmail.com>
2021-07-30 20:19:43 -0300

    .

--- SistemaPadaria/SistemaPadaria/Models/Estoque.cs
+++ SistemaPadaria/SistemaPadaria/Models/Estoque.cs
@@ -1,18 +1,49 @@
 using System.Collections.Generic;
+using System.Linq;
 namespace SistemaPadaria.Models
 {
     public class Estoque
     {
-        public Estoque(int id, float pesoTotal, int quantidade)
+        public int Id { get; private set; }
+        public float PesoTotal { get; private set; }
+        public float Quantidade { get; private set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+
+        public List<Estoque> listaEstoque { get; set; }
+        
+        public void Inserir(int id, float pesoTotal, int quantidade)
         {
             this.Id = id;
             this.PesoTotal = pesoTotal;
             this.Quantidade = quantidade;
+        }
+
+        public void SubtrairEstoque(float itemReceita)
+        {
+            Quantidade -= itemReceita;
+        }
+
+        public void conversãoMedida() 
+        {
 
         }
-        public int Id { get; private set; }
-        public float PesoTotal { get; private set; }
-        public int Quantidade { get; private set; }
-        public IEnumerable<Produto> Produtos { get; set; }
+        public Estoque ConsultarPorId(int id)
+        {
+            return listaEstoque.FirstOrDefault(x => x.Id == id);
+        }
+        public List<Estoque> ConsultarListaEstoque() 
+        {
+            return listaEstoque.OrderBy(x => x.Produtos).ToList();
+        }
+        public void editar(int quantidade, float pesoTotal, int id) 
+        {
+            Id = Id;
+            Quantidade = (quantidade <= 0) ? Quantidade : quantidade;
+            PesoTotal = (pesoTotal <= 0) ? PesoTotal : pesoTotal;
+        }
+        public void deletar() 
+        {
+
+        }
     }
 }
